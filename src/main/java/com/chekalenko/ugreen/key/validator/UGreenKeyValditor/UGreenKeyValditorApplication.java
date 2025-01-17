3b18ae5ac908ff7cfee9d46a0138dfdde2ec5e86
vitalii.chekalenko <vitalii.chekalenko@techtellent.io>
2025-01-17 09:28:23 +0100

    Initial Commit

---
+++ src/main/java/com/chekalenko/ugreen/key/validator/UGreenKeyValditor/UGreenKeyValditorApplication.java
@@ -0,0 +1,13 @@
+package com.chekalenko.ugreen.key.validator.UGreenKeyValditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UGreenKeyValditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UGreenKeyValditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chekalenko/ugreen/key/validator/UGreenKeyValditor/UGreenKeyValditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chekalenko.ugreen.key.validator.UGreenKeyValditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UGreenKeyValditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
