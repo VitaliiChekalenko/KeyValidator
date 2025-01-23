1be4285f16457963324e7583d60567cef852d2bd
vitalii.chekalenko <vitalii.chekalenko@techtellent.io>
2025-01-23 23:07:19 +0100

    [CC-2] - Code review

--- src/main/java/com/chekalenko/ugreen/key/validator/UGreenKeyValditor/controllers/LicenseController.java
+++ src/main/java/com/chekalenko/ugreen/key/validator/UGreenKeyValditor/controllers/LicenseController.java
@@ -2,17 +2,13 @@ package com.chekalenko.ugreen.key.validator.UGreenKeyValditor.controllers;
 
 
 import com.chekalenko.ugreen.key.validator.UGreenKeyValditor.model.LicenseKey;
-import com.chekalenko.ugreen.key.validator.UGreenKeyValditor.repos.LicenseKeyRepository;
 import com.chekalenko.ugreen.key.validator.UGreenKeyValditor.service.LicenseValidationService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
 
 @RestController
 @AllArgsConstructor
@@ -20,29 +16,15 @@ import java.time.LocalDate;
 public class LicenseController {
 
     private final LicenseValidationService licenseValidationService;
-    private final LicenseKeyRepository licenseRepository;
 
     @PostMapping("/validate")
     public ResponseEntity<String> validateLicense(@RequestBody LicenseRequest licenseRequest) {
-        String isValid = licenseValidationService.validateLicenseKey(licenseRequest.getKey());
-        return ResponseEntity.ok(isValid);
+        return ResponseEntity.ok(licenseValidationService.validateLicenseKey(licenseRequest.getKey()));
     }
 
     @PostMapping("/add")
     public ResponseEntity<String> addLicenseKey(@RequestBody LicenseKey license) {
-        try {
-            LicenseKey key = new LicenseKey();
-            key.setLicenseKey(license.getLicenseKey());
-            key.setActivated(false);
-            key.setActive(true);
-            key.setExpDate(LocalDate.now().plusDays(2));
-            licenseRepository.save(license);
-        } catch (Exception e) {
-            if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
-                return ResponseEntity.status(HttpStatus.CONFLICT)
-                        .body("License key already exists: " + license.getLicenseKey());
-            }
-        }
+        licenseValidationService.addLicenseKey(license);
         return ResponseEntity.ok("License key added successfully.");
     }
 
--- src/main/java/com/chekalenko/ugreen/key/validator/UGreenKeyValditor/service/LicenseValidationService.java
+++ src/main/java/com/chekalenko/ugreen/key/validator/UGreenKeyValditor/service/LicenseValidationService.java
@@ -1,9 +1,11 @@
 package com.chekalenko.ugreen.key.validator.UGreenKeyValditor.service;
 
+import com.chekalenko.ugreen.key.validator.UGreenKeyValditor.model.LicenseKey;
 import com.chekalenko.ugreen.key.validator.UGreenKeyValditor.repos.LicenseKeyRepository;
-import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 @AllArgsConstructor
@@ -12,17 +14,28 @@ public class LicenseValidationService {
     private final LicenseKeyRepository licenseRepository;
 
     public String validateLicenseKey(String key) {
-        boolean isValid = licenseRepository.findByLicenseKey(key).isPresent();
-        return isValid ? "License key is valid." : "Invalid license key.";
+        return licenseRepository.findByLicenseKey(key).isPresent() ?
+                "License key is valid." : "Invalid license key.";
     }
 
-    @Transactional
     public String clearAllLicenses() {
         try {
-            licenseRepository.deleteAllEntries();
+            licenseRepository.removeAllLicenseKey();
         } catch (Exception e) {
             return e.getMessage();
         }
         return "DB is cleared";
+    }
+
+    public void addLicenseKey(LicenseKey license) {
+        if (licenseRepository.findByLicenseKey(license.getLicenseKey()).isPresent()) {
+            throw new RuntimeException("Key already exist");
+        }
+
+        license.setLicenseKey(license.getLicenseKey());
+        license.setActivated(false);
+        license.setActive(true);
+        license.setExpDate(LocalDate.now().plusDays(2));
+        licenseRepository.save(license);
     }
 }
