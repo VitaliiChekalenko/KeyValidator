2f246a774060f20792f93eb76e9dfb916bfad79f
vitalii.chekalenko <vitalii.chekalenko@techtellent.io>
2025-02-13 21:45:21 +0100

    [CC-5] - Tests

--- src/test/java/com/chekalenko/ugreen/key/validator/tests/UGreenKeyValditorApplicationTests.java
+++ src/test/java/com/chekalenko/ugreen/key/validator/tests/UGreenKeyValditorApplicationTests.java
@@ -3,8 +3,13 @@ package com.chekalenko.ugreen.key.validator.tests;
 import com.chekalenko.key.validator.UGreenKeyValditorApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(classes = UGreenKeyValditorApplication.class)
+@TestPropertySource(properties = {
+		"spring.datasource.username=${DB_USERNAME}",
+		"spring.datasource.password=${DB_PASSWORD}"
+})
 class UGreenKeyValditorApplicationTests {
 
 	@Test
